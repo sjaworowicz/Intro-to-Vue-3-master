561303b3eefaee26157a56be42f63dcf04939a7d
sjaworowicz <125sjaworowicz@frhsd.com>
2025-02-12 09:47:38 -0500

    Lesson 5 complete

--- Intro-to-Vue-3-master/main.js
+++ Intro-to-Vue-3-master/main.js
@@ -3,7 +3,11 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 8
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'},
+            ]
         }
     }
 })
