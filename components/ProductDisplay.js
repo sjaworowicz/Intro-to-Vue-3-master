3a20d897d615562d3a5699ad3594676a02c0e433
sjaworowicz <125sjaworowicz@frhsd.com>
2025-02-18 08:35:09 -0500

    lesson 11 complete

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -27,6 +27,7 @@ template:
       <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" @click="addToCart">Add to Cart</button>
     </div>
    </div>
+   <review-form @review-submitted="addReview"></review-form>
   </div>`,
   data() {
     return {
@@ -37,7 +38,8 @@ template:
         variants: [
             { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
             { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
-        ]
+        ],
+        reviews: []
     }
 },
 methods: {
@@ -46,6 +48,9 @@ methods: {
     },
     updateVariant(index) {
         this.selectedVariant = index
+    },
+    addReview(review) {
+        this.reviews.push(review)
     }
 },
 computed: {
